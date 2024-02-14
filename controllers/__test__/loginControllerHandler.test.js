e45b14f3731a44cc063a6f8fd6df4dfe3510c99c
endryuman <132812966+endryuman@users.noreply.github.com>
2024-02-14 12:30:09 +0200

    bag fix

---
+++ controllers/__test__/loginControllerHandler.test.js
@@ -0,0 +1,7 @@
+// import { login } from "../authController.js";
+
+// describe("Login controller tests", () => {
+//   test("return status 200", () => {
+//     expect(login(req, res, next));
+//   });
+// });
